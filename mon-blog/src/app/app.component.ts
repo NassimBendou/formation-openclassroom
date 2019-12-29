0d8ac87a07ee28f2da509958aebf28dba421297a
Nassim BENDOU <nassim.bendou@outlook.fr>
2019-12-29 14:47:43 +0100

    [mon-blog] mise en place de ng-for dans app

--- mon-blog/src/app/app.component.ts
+++ mon-blog/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-blog';
+
+  posts=[
+  {
+      title: "Mon premier post",
+      content: "Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo"
+                +"erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria"
+                +"excellens de amicitia loquetur.",
+      loveIts: 0,
+      created_at: new Date()
+  },
+  {
+      title: "Mon deuxième post",
+      content: "Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo"
+                +"erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria"
+                +"excellens de amicitia loquetur.",
+      loveIts: 0,
+      created_at: new Date()
+  }
+  ]
+
 }
+
+
+
+
