358580cc55242e0e0fd7e99495934e9f9222585b
Nassim BENDOU <nassim.bendou@outlook.fr>
2020-01-04 15:26:23 +0100

    [mon-projet-angular] fin du chapitre : écouter l'utilisateur avec les forms - méthode réactive

--- mon-projet-angular/src/app/app.module.ts
+++ mon-projet-angular/src/app/app.module.ts
@@ -13,13 +13,17 @@ import { SingleAppareilComponent } from './single-appareil/single-appareil.compo
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
-
-
+import { UserService } from './services/user.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
 const appRoutes: Routes = [
-{ path: 'appareils',canActivate : [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils',canActivate : [AuthGuard], component: AppareilViewComponent },
   { path: 'appareils/:id',canActivate : [AuthGuard] , component: SingleAppareilComponent },
   { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'new-user', component: NewUserComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
   { path: '**', redirectTo: 'not-found' }];
@@ -32,14 +36,17 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
   ],
-  providers: [AppareilService, AuthService, AuthGuard],
+  providers: [AppareilService, AuthService, AuthGuard, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
