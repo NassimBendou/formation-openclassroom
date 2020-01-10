a453da9e7be998c25cbcfa25c47d26cec64fcce2
Nassim BENDOU <nassim.bendou@outlook.fr>
2020-01-10 16:00:22 +0100

    [ma-bibliotheque] toute l'application

--- ma-bibliotheque/src/app/app.component.ts
+++ ma-bibliotheque/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ma-bibliotheque';
+  constructor() {
+    const config = {
+        apiKey: "AIzaSyBZnEE2nDd6JIfuVcOaV7jqKlZvZSQXk4k",
+        authDomain: "ma-bibliotheque-add8c.firebaseapp.com",
+        databaseURL: "https://ma-bibliotheque-add8c.firebaseio.com",
+        projectId: "ma-bibliotheque-add8c",
+        storageBucket: "ma-bibliotheque-add8c.appspot.com",
+        messagingSenderId: "244477424046",
+        appId: "1:244477424046:web:a59e0053bc569bb18ed83f"
+    };
+    firebase.initializeApp(config);
+  }
 }
